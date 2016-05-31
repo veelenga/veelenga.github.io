2a9dea70915c1c4fb3db000ef740abc53a9bd068
Vitalii Elenhaupt <velenhaupt@gmail.com>
2016-05-31 10:15:50 +0300

    Fix tag list

--- _plugins/tag_helper_filter.rb
+++ _plugins/tag_helper_filter.rb
@@ -1,7 +1,7 @@
 module Jekyll
   module TagHelpersFilter
     def sort_tags_by_name(tags)
-      tags_only(tags).sort_by { |x| [ x[0].downcase ] }
+      tags.sort_by { |x| [ x[0].downcase ] }
     end
 
     def tags_only(tags)
